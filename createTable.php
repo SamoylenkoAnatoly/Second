13404a5fe47c09791c4f0a5b4cf616e75a5fd86f
Volunter <homerjsimpsons1998@gmail.com>
2017-09-21 14:00:56 +0300

    Update createTable.php

--- createTable.php
+++ createTable.php
@@ -1,6 +1,6 @@
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:se4.database.windows.net,1433; Database = НоваяБД", "vol", "{Simpsons1}");
+    $conn = new PDO("sqlsrv:server = tcp:se4.database.windows.net,1433; Database = НоваяБД", "vol", "Simpsons1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE registration_tbl(
     id INT NOT NULL IDENTITY(1,1) 
@@ -17,6 +17,6 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "vol@se4", "pwd" => "{your_password_here}", "Database" => "НоваяБД", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "vol@se4", "pwd" => "Simpsons1", "Database" => "НоваяБД", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:se4.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
