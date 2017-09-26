aa33f98c7eb66b8bede03ee3b3be08044de5bac0
Volunter <homerjsimpsons1998@gmail.com>
2017-09-26 10:05:56 +0300

    Update createTable.php

--- createTable.php
+++ createTable.php
@@ -36,30 +36,6 @@ name="email" id="email"/></br>
       <input type="submit" 
 name="submit" value="Submit" />
 </form>
-<?php
-// DB connection info
-$host = "localhost\sqlexpress";
-$user =  "vol1";
-$pwd = "Simpsons1";
-$db = "NewBD";
-try{
-    $conn = new PDO
-( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    $conn->setAttribute
-( PDO::ATTR_ERRMODE, 
-PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE registration_tbl(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
-}
-catch(Exception $e){
-    die(print_r($e));
-}
-echo "<h3>Table created.</h3>";
-?>
+
 </body>
 </html>
